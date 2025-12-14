149a3c1aa9fcd376ca7990fb79ab11f20d6ffe07
lautaroh2394 <lautarohaase_23@hotmail.com>
2025-12-14 16:58:29 -0300

    rename files. add index.html

--- scenes/game_over.gd
+++ scenes/game_over.gd
@@ -5,12 +5,14 @@ var level_scene: PackedScene = load("res://scenes/level.tscn")
 func _ready() -> void:
 	$CenterContainer/VBoxContainer/Label2.text = "\nPuntaje: " + str(Global.score)
 	$CenterContainer/VBoxContainer/Label2.text += "\nLauti arrojó " + str(Global.total_shot) + " corazones\nAcertó " + str(Global.total_hit) + "\n"
-	if Global.total_hit == Global.total_shot:
+	if Global.total_hit >= 30:
 		$CenterContainer/VBoxContainer/Label3.visible = true
 		$AudioStreamPlayer.play()
 		
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot"):
+		Global.total_hit = 0
+		Global.total_shot = 0
 		get_tree().change_scene_to_packed(level_scene)
 
 
